218a3ea94ebd543cbfa0e36b7c7b827130b50b50
Steven Tan <szt4@scarletmail.rutgers.edu>
2023-10-22 16:24:56 -0400

    Finished detect algorithm for bot1

--- bot1.py
+++ bot1.py
@@ -7,16 +7,12 @@ from collections import deque
 from colorama import init, Back, Style
 init(autoreset=True)
 
-#Goal of Bot1 is to find the leak
-#In order to win, Bot1 must make sure to enter a cell that is considered "leaked"
-
 DIRECTIONS = [0, 1, 0, -1, 0]
-D = 50#Set dimensions to 50
-
-potentialLeakset = open_cells.copy()
+D = 5
 
 
-def has_one_open_neighbor(ship, r, c):#This is basically saying that the cell has exactly one open neighbor
+# This function checks if a cell has exactly one open neighbor
+def has_one_open_neighbor(ship, r, c):
     num_open_neighbors = 0
     for i in range(4):
         nx, ny = DIRECTIONS[i] + r, DIRECTIONS[i + 1] + c
@@ -25,8 +21,9 @@ def has_one_open_neighbor(ship, r, c):#This is basically saying that the cell ha
         num_open_neighbors += 1
     return num_open_neighbors <= 1
 
+
 # Finds all the deadends located in the ship
-def find_deadends(ship, open_cells, deadends):#Basically if an open cell has one neighbor, those are considered deadends and basically adds that to deadends set
+def find_deadends(ship, open_cells, deadends):
     for curr_x, curr_y in open_cells:
         for i in range(4):
             nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
@@ -38,26 +35,25 @@ def find_deadends(ship, open_cells, deadends):#Basically if an open cell has one
 # Creates the ship with logic in correspondence to the assignment write-up
 def create_ship(ship, blocked_one_window_cells, open_cells):
     while blocked_one_window_cells:
-        curr_x, curr_y = random.choice(list(blocked_one_window_cells))#get a random point from blocked cells
-        blocked_one_window_cells.remove((curr_x, curr_y))#remove it
-        if not has_one_open_neighbor(ship, curr_x, curr_y):#if it doesn't have one open neighbor then iterate through the next thing
+        curr_x, curr_y = random.choice(list(blocked_one_window_cells))
+        blocked_one_window_cells.remove((curr_x, curr_y))
+        if not has_one_open_neighbor(ship, curr_x, curr_y):
             continue
         ship[curr_x][curr_y] = 0
-        open_cells.add((curr_x, curr_y))#Consider this point an open cell if it does have exactly one open neighbor
+        open_cells.add((curr_x, curr_y))
         for i in range(4):
             nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
             if nx in [-1, D] or ny in [-1, D] or ship[nx][ny] == 0:
                 continue
             if has_one_open_neighbor(ship, nx, ny):
-                blocked_one_window_cells.add((nx, ny))#Get the next blocked cell that has one open neighbor
-
+                blocked_one_window_cells.add((nx, ny))
 
     deadends = set()
     find_deadends(ship, open_cells, deadends)
 
     length = len(deadends)
     for i in range(length // 2):
-        deadend_x, deadend_y = random.choice(list(deadends))#deadendx is 
+        deadend_x, deadend_y = random.choice(list(deadends))
         blocked_cells = set()
         for i in range(4):
             nx, ny = DIRECTIONS[i] + deadend_x, DIRECTIONS[i + 1] + deadend_y
@@ -71,109 +67,80 @@ def create_ship(ship, blocked_one_window_cells, open_cells):
         deadends.remove((deadend_x, deadend_y))
 
 
-def search(ship, curr_x, curr_y, goal, open_cells, D):
-    kmax = (D/2)-1
-    
-    while k % 2 == 0 or k < 1 or k > kmax:#makes sure that k is odd
-        k =random.randint(0,kmax)#0-49
-        
-    
-    leakSet = set()
-    
-    temp_x = curr_x-k#-1
-    length = 2*k+1
+def detect(ship, curr_x, curr_y, leak, potential_leaks, K):
+    found_leak = False
+    cells_detected = set()
+    for r in range(curr_x - K, curr_x + K + 1):
+        for c in range(curr_y - K, curr_y + K + 1):
+            if r <= -1 or r >= D or c <= -1 or c >= D or ship[r][c] == 1 or (r, c) not in potential_leaks:
+                continue
+
+            if (r, c) == leak:
+                found_leak = True
+            cells_detected.add((r, c))
+
+    if found_leak:
+        potential_leaks.clear()
+        potential_leaks.update(cells_detected)
+        print(potential_leaks)
+    else:
+        for coordinate in cells_detected:
+            potential_leaks.remove(coordinate)
+    return potential_leaks
 
 
-    for i in range(length):
-        temp_y = curr_y-k#-1
-
-        for j in range(length):
-            if ship[temp_x][temp_y] == None or ship[temp_x][temp_y] == 1:#make sure that it is not out of bounds or closed
-                temp_y += 1
-
-            elif (temp_x, temp_y) == goal:
-                return
-
-            else:
-                leakSet.add((temp_x, temp_y))
-                temp_y += 1
-
-        temp_x = temp_x+1
-
-    for coordinate in leakSet: 
-        potentialLeakset.remove(coordinate)
-    #leakSet.clear()#clear the leakset because we did not get the goal
-    return leakSet
-
-def move(ship, start_x, start_y, goal, D):
-    fringe = deque([(start_x, start_y)])
-    closed_set = set()
-    previous_state = {(start_x, start_y): None}
-
-    while fringe:
-        curr_x, curr_y = fringe.popleft()
-        if (curr_x, curr_y) == goal:
-            path, coord = [], (curr_x, curr_y)
-            while coord != None:
-                path.append(coord)
-                coord = previous_state[coord]
-
-            return path[::-1]
-        
-        leakset = set()
-
-        leakset = search(ship, curr_x, curr_y, goal, leakset, D)
-
-        if len(leakset) != 0:#if we realize that hey there is a cell in a (2k+1) * (2k+1) square that might have the leak
-            for i in range(4):
-
-                nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
-                if nx in [-1, D] or ny in [-1, D] or ship[nx][ny] != 0 or (nx, ny) in closed_set or (nx, ny) not in leakset:
-                    continue
-                fringe.append((nx, ny))
-                previous_state[(nx, ny)] = (curr_x, curr_y)
-                closed_set.add((nx, ny))
-            closed_set.add((curr_x, curr_y))
-                
-            
-
-        else:#if we don't recognize any cell that has leak, then just keep doing the normal thing that Bot1 would do in project1
-            for i in range(4):
-                nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
-                if nx in [-1, D] or ny in [-1, D] or ship[nx][ny] != 0 or (nx, ny) in closed_set:
-                    continue
-                fringe.append((nx, ny))
-                previous_state[(nx, ny)] = (curr_x, curr_y)
-                closed_set.add((nx, ny))
-            closed_set.add((curr_x, curr_y))
-    return None
+def move(ship, curr_x, curr_y, leak, potential_leaks, K):
+    potential_leaks = detect(ship, curr_x, curr_y, leak, potential_leaks, K)
+    # leakset = search(ship, curr_x, curr_y, goal, leakset, K)
+    #
+    # if len(leakset) != 0:#if we realize that hey there is a cell in a (2k+1) * (2k+1) square that might have the leak
+    #     for i in range(4):
+    #
+    #         nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
+    #         if nx in [-1, D] or ny in [-1, D] or ship[nx][ny] != 0 or (nx, ny) in closed_set or (nx, ny) not in leakset:
+    #             continue
+    #         fringe.append((nx, ny))
+    #         previous_state[(nx, ny)] = (curr_x, curr_y)
+    #         closed_set.add((nx, ny))
+    #     closed_set.add((curr_x, curr_y))
+    # else:#if we don't recognize any cell that has leak, then just keep doing the normal thing that Bot1 would do in project1
+    #     for i in range(4):
+    #         nx, ny = DIRECTIONS[i] + curr_x, DIRECTIONS[i + 1] + curr_y
+    #         if nx in [-1, D] or ny in [-1, D] or ship[nx][ny] != 0 or (nx, ny) in closed_set:
+    #             continue
+    #         fringe.append((nx, ny))
+    #         previous_state[(nx, ny)] = (curr_x, curr_y)
+    #         closed_set.add((nx, ny))
+    #     closed_set.add((curr_x, curr_y))
+    # return None
 
 
-    
+def run_bot1():
+    # ship = [[1 for i in range(D)] for j in range(D)]
+    # start_x, start_y = random.randint(0, D - 1), random.randint(0, D - 1)   # start coordinates for the bot
+    # ship[start_x][start_y], open_cells = 0, set()
+    # blocked_one_window_cells = {(start_x, start_y)}
+    # create_ship(ship, blocked_one_window_cells, open_cells)
+    ship = [[1,1,0,0,0], [0,0,0,0,0], [0,1,0,0,1], [0,0,0,0,0], [0,0,0,0,0]]
+    start_x, start_y = 3, 3
+    open_cells = set()
+    K = (D // 2) - 1
 
-   
+    for i in range(D):
+        print(ship[i])
+    print()
+    leak_cell = (4, 4)
+    # leak_cell = random.choice(list(open_cells))
+    potential_leaks = open_cells.copy()
+    for i in range(D):
+        for j in range(D):
+            if ship[i][j] == 0:
+                potential_leaks.add((i, j))
+    num_moves = move(ship, start_x, start_y, leak_cell, potential_leaks, K)
 
-
-def main():
-    ship = [[1 for i in range(D)] for j in range(D)]
-    start_x, start_y = random.randint(0, D - 1), random.randint(0, D - 1)   # start coordinates for the bot
-    ship[start_x][start_y], open_cells = 0, set()
-    blocked_one_window_cells = {(start_x, start_y)}
-    leakset = set()
-
-    create_ship(ship, blocked_one_window_cells, open_cells)
-
-    leakcell = random.choice(list(open_cells))# start coordinates for the leak
-
-    path = move(ship, start_x, start_y, leakcell, leakset, D)
-
-    
-    print(path)
-
-    
-
-
+    return num_moves
 
 
 if __name__ == '__main__':
-    main()
+    total_moves = 0
+    run_bot1()
